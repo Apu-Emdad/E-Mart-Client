2ebd53d4d56b31a9e1224e5480b0b1502e467bef
ApuEmdad <emdadul.apu@savethechildren.org>
2023-08-16 13:49:51 +0600

    Avatar button is working username button is working properly

--- src/Components/MuiHeader.jsx
+++ src/Components/MuiHeader.jsx
@@ -31,6 +31,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Avatar, ListItemIcon, styled } from "@material-ui/core";
 import { Logout } from "@mui/icons-material";
+import { logOut } from "../Redux/Slices/userSlice";
 const MuiHeader = () => {
   const totalOrders = useSelector((state) => state.cart.totalOrders);
   const user = useSelector((state) => state.user.currentUser);
@@ -39,18 +40,26 @@ const MuiHeader = () => {
 
   const [mobileOpen, setMobileOpen] = useState(false);
   const [anchorElUser, setAnchorElUser] = useState(null);
+  const open = Boolean(anchorElUser);
   const isWideScreen = useMediaQuery("(min-width: 960px)");
   const navigate = useNavigate();
 
   const handleOpenUserMenu = (event) => {
-    console.log("handleOpenUserMenu");
     setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseUserMenu = () => {
-    console.log("handleCloseUserMenu");
-
     setAnchorElUser(null);
+  };
+
+  const handleLogOut = () => {
+    dispatch(logOut());
+  };
+
+  console.log("open:", open);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
   };
 
   const noUserNavItems = [
@@ -68,12 +77,28 @@ const MuiHeader = () => {
     },
   ];
 
-  const handleNoUserDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
+  /* ==== Styled Component Starts ==== */
+  const TitleContainer = styled(Box)({
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: isWideScreen ? "flex-start" : "center",
+  });
+  const Title = styled(Typography)({
+    fontSize: "40px",
+    fontWeight: "bold",
+    cursor: "pointer",
+    paddingRight: !isWideScreen && "70px",
+    paddingLeft: isWideScreen && "8rem",
+  });
+
+  const NavItemContainer = styled(Box)({
+    display: isWideScreen ? "block" : "none",
+    paddingRight: "5rem",
+  });
+  /* ==== Styled Component Ends ==== */
 
   const NoUserDrawer = (
-    <Box onClick={handleNoUserDrawerToggle} sx={{ textAlign: "center" }}>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
       <Box
         sx={{
           display: "flex",
@@ -104,26 +129,39 @@ const MuiHeader = () => {
       </List>
     </Box>
   );
-  /* ==== Styled Component Starts ==== */
-  const TitleContainer = styled(Box)({
-    flexGrow: 1,
-    display: "flex",
-    justifyContent: isWideScreen ? "flex-start" : "center",
-  });
-  const Title = styled(Typography)({
-    fontSize: "40px",
-    fontWeight: "bold",
-    cursor: "pointer",
-    paddingRight: !isWideScreen && "70px",
-    paddingLeft: isWideScreen && "8rem",
-  });
 
-  const NavItemContainer = styled(Box)({
-    display: isWideScreen ? "block" : "none",
-    paddingRight: "5rem",
-  });
-
-  /* ==== Styled Component Ends ==== */
+  const UserDrawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "0px 10px",
+        }}
+      >
+        <Typography variant="h6" sx={{ my: 2, fontWeight: "Bold" }}>
+          E-mart
+        </Typography>
+        <IconButton>
+          <Close />
+        </IconButton>
+      </Box>
+      <Divider />
+      <List>
+        {/* {noUserNavItems.map((item) => (
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton
+              sx={{ textAlign: "center" }}
+              onClick={() => navigate(item.route)}
+            >
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))} */}
+      </List>
+    </Box>
+  );
 
   const NoUserNavbar = () => (
     <Box sx={{ display: "flex" }}>
@@ -133,7 +171,7 @@ const MuiHeader = () => {
       >
         <Toolbar>
           <IconButton
-            onClick={handleNoUserDrawerToggle}
+            onClick={handleDrawerToggle}
             sx={{ mr: 2, display: isWideScreen && "none" }}
           >
             <MenuIcon />
@@ -165,7 +203,7 @@ const MuiHeader = () => {
         <Drawer
           variant="temporary"
           open={mobileOpen}
-          onClose={handleNoUserDrawerToggle}
+          onClose={handleDrawerToggle}
           ModalProps={{
             keepMounted: true,
           }}
@@ -192,7 +230,7 @@ const MuiHeader = () => {
       >
         <Toolbar>
           <IconButton
-            onClick={handleNoUserDrawerToggle}
+            onClick={handleDrawerToggle}
             sx={{ mr: 2, display: isWideScreen && "none" }}
           >
             <MenuIcon />
@@ -223,89 +261,94 @@ const MuiHeader = () => {
                 </Badge>
               </IconButton>
             </Link>
-            <Button onClick={handleOpenUserMenu}>
+            {/* ==== userMenu starts ====*/}
+            <Button
+              onClick={handleOpenUserMenu}
+              sx={{ paddingRight: "0px", paddingLeft: "20px" }}
+            >
               <Typography variant="span" fontWeight="bold">
                 {user.username}
               </Typography>
             </Button>
-            <IconButton onClick={handleOpenUserMenu}>
-              <Avatar src="https://i.ibb.co/sRr8kWj/avatar.png" alt="" />
-              <Menu
-                sx={{ mt: "45px", left: "calc(100vw - 18rem) !important" }}
-                anchorEl={anchorElUser}
-                // keepMounted
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                <Link to="/dashboard">
-                  <MenuItem onClick={handleCloseUserMenu}>
-                    <ListItemIcon>
-                      <LineStyle />
-                    </ListItemIcon>
-                    <ListItemText sx={{ color: "black" }}>
-                      My Dashboard
-                    </ListItemText>
-                  </MenuItem>
-                </Link>
-                {isAdmin ? (
-                  <div>
-                    <Link to="/dashboard/orders">
-                      <MenuItem onClick={handleCloseUserMenu}>
-                        <ListItemIcon>
-                          <ListAltOutlined />
-                        </ListItemIcon>
-                        <ListItemText sx={{ color: "black" }}>
-                          Orders
-                        </ListItemText>
-                      </MenuItem>
-                    </Link>
-                    <Link to="/dashboard/productTable">
-                      <MenuItem onClick={handleCloseUserMenu}>
-                        <ListItemIcon>
-                          <Storefront />
-                        </ListItemIcon>
-                        <ListItemText sx={{ color: "black" }}>
-                          Products
-                        </ListItemText>
-                      </MenuItem>
-                    </Link>
-                  </div>
-                ) : (
-                  <div>
-                    <Link to="/dashboard/myorders">
-                      <MenuItem onClick={handleCloseUserMenu}>
-                        <ListItemIcon>
-                          <ListAltOutlined />
-                        </ListItemIcon>
-                        <ListItemText sx={{ color: "black" }}>
-                          My Orders
-                        </ListItemText>
-                      </MenuItem>
-                    </Link>
-                    <Link to={`/dashboard/user/${user._id}`}>
-                      <MenuItem onClick={handleCloseUserMenu}>
-                        <ListItemIcon>
-                          <AccountCircleOutlined />
-                        </ListItemIcon>
-                        <ListItemText sx={{ color: "black" }}>
-                          Edit Profile
-                        </ListItemText>
-                      </MenuItem>
-                    </Link>
-                  </div>
-                )}
+            <Button onClick={handleOpenUserMenu}>
+              <Avatar src="https://i.ibb.co/sRr8kWj/avatar.png" />
+            </Button>
+            <Menu
+              sx={{ mt: "45px", left: "calc(100vw - 18rem) !important" }}
+              anchorEl={anchorElUser}
+              open={open}
+              onClose={handleCloseUserMenu}
+            >
+              <Link to="/dashboard">
                 <MenuItem onClick={handleCloseUserMenu}>
-                  <Button
-                    variant="contained"
-                    color="warning"
-                    sx={{ margin: "0 auto", fontWeight: "bold" }}
-                  >
-                    <Logout />
-                    &nbsp;Log Out
-                  </Button>
+                  <ListItemIcon>
+                    <LineStyle />
+                  </ListItemIcon>
+                  <ListItemText sx={{ color: "black" }}>
+                    My Dashboard
+                  </ListItemText>
                 </MenuItem>
-              </Menu>
-            </IconButton>
+              </Link>
+              {isAdmin ? (
+                <div>
+                  <Link to="/dashboard/orders">
+                    <MenuItem onClick={handleCloseUserMenu}>
+                      <ListItemIcon>
+                        <ListAltOutlined />
+                      </ListItemIcon>
+                      <ListItemText sx={{ color: "black" }}>
+                        Orders
+                      </ListItemText>
+                    </MenuItem>
+                  </Link>
+                  <Link to="/dashboard/productTable">
+                    <MenuItem onClick={handleCloseUserMenu}>
+                      <ListItemIcon>
+                        <Storefront />
+                      </ListItemIcon>
+                      <ListItemText sx={{ color: "black" }}>
+                        Products
+                      </ListItemText>
+                    </MenuItem>
+                  </Link>
+                </div>
+              ) : (
+                <div>
+                  <Link to="/dashboard/myorders">
+                    <MenuItem onClick={handleCloseUserMenu}>
+                      <ListItemIcon>
+                        <ListAltOutlined />
+                      </ListItemIcon>
+                      <ListItemText sx={{ color: "black" }}>
+                        My Orders
+                      </ListItemText>
+                    </MenuItem>
+                  </Link>
+                  <Link to={`/dashboard/user/${user._id}`}>
+                    <MenuItem onClick={handleCloseUserMenu}>
+                      <ListItemIcon>
+                        <AccountCircleOutlined />
+                      </ListItemIcon>
+                      <ListItemText sx={{ color: "black" }}>
+                        Edit Profile
+                      </ListItemText>
+                    </MenuItem>
+                  </Link>
+                </div>
+              )}
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Button
+                  variant="contained"
+                  color="warning"
+                  sx={{ margin: "0 auto", fontWeight: "bold" }}
+                  onClick={handleLogOut}
+                >
+                  <Logout />
+                  &nbsp;Log Out
+                </Button>
+              </MenuItem>
+            </Menu>
+            {/* ==== userMenu ends ====*/}
           </NavItemContainer>
           {/* ==== Nav Item Ends ==== */}
         </Toolbar>
@@ -314,7 +357,7 @@ const MuiHeader = () => {
         <Drawer
           variant="temporary"
           open={mobileOpen}
-          onClose={handleNoUserDrawerToggle}
+          onClose={handleDrawerToggle}
           ModalProps={{
             keepMounted: true,
           }}
@@ -327,7 +370,7 @@ const MuiHeader = () => {
             },
           }}
         >
-          {NoUserDrawer}
+          {UserDrawer}
         </Drawer>
       </Box>
     </Box>
